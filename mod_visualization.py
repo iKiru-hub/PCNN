d8bd42b46a1e6d12ebd1151c32bd4c0013de5305
iKiru-hub <dkirubel@gmail.com>
2023-11-06 18:44:43 +0100

    online update

---
+++ mod_visualization.py
@@ -0,0 +1,117 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import networkx as nx
+
+
+def plot_graph_colored(W: np.ndarray):
+
+    """
+    Plot a graph of the neurons with edges colored according to their weight.
+
+    Parameters
+    ----------
+    W : np.ndarray
+        The weight matrix of the network.
+    """
+
+    # Create a graph
+    G = nx.Graph()
+
+    # Add nodes
+    G.add_nodes_from(range(len(W)))
+
+    # Add weighted edges
+    for i in range(len(W)):
+        for j in range(i+1, len(W)):  # Only upper triangle needed for undirected graph
+            weight = W[i, j]
+            G.add_edge(i, j, weight=weight)
+
+    # Position nodes in a circle
+    pos = nx.circular_layout(G)
+
+    # Draw the nodes
+    nx.draw_networkx_nodes(G, pos, node_color='k', node_size=50)
+
+    # Draw the edges with a color map
+    edges = G.edges(data=True)
+    colors = [d['weight'] for (u, v, d) in edges]
+    nx.draw_networkx_edges(G, pos, edgelist=edges, edge_color=colors,
+                           edge_cmap=plt.cm.RdYlGn, edge_vmin=-1, edge_vmax=1)
+
+    # Show the plot
+    plt.title('Network Graph with Weight-Colored Connections')
+    plt.axis('off')
+    plt.show()
+
+
+
+# Define a function to plot the graph with a 2D structure
+def plot_graph_colored_2D(W: np.ndarray, grid_dimensions, threshold=0.):
+
+    """
+    Plot a graph of the neurons in a 2D grid with edges colored according to their weight.
+
+    Parameters
+    ----------
+    W : np.ndarray
+        The weight matrix of the network.
+    grid_dimensions : tuple
+        The dimensions of the grid.
+    threshold : float
+        The threshold for the weight to be considered significant.
+    """
+
+    rows, cols = grid_dimensions
+    N = rows * cols
+
+    # Create a graph
+    G = nx.Graph()
+
+    # Add nodes with 2D positions
+    for i in range(N):
+        x, y = i // cols, i % cols  # Convert index to 2D position
+        G.add_node(i, pos=(x, y))
+
+    # Add weighted edges considering the threshold
+    for i in range(N):
+        for j in range(i+1, N):  # Only upper triangle needed for undirected graph
+            weight = W[i, j]
+            if abs(weight) > threshold:  # Only consider significant weights
+                G.add_edge(i, j, weight=weight)
+
+    # Get positions from node data
+    pos = nx.get_node_attributes(G, 'pos')
+
+    # Draw the nodes
+    nx.draw_networkx_nodes(G, pos, node_color='k', node_size=100)
+
+    # Draw the edges with a color map
+    edges = G.edges(data=True)
+    colors = [d['weight'] for (u, v, d) in edges]
+    nx.draw_networkx_edges(G, pos, edgelist=edges, edge_color=colors,
+                           edge_cmap=plt.cm.RdYlGn, edge_vmin=-1, edge_vmax=1)
+
+    # Show the plot with color bar
+    plt.title('2D Grid Graph with Weight-Colored Connections')
+    plt.axis('equal')  # Ensure x and y scales are equal to avoid distortion
+    plt.axis('off')  # Turn off the axis
+    plt.show()
+
+
+def plot_weight_matrix(W: np.ndarray):
+
+    """
+    Plot the weight matrix as a heatmap.
+
+    Parameters
+    ----------
+    W : np.ndarray
+        The weight matrix of the network.
+    """
+
+    plt.imshow(W, cmap='RdYlGn', vmin=-1, vmax=1)
+    plt.title('Weight Matrix')
+    plt.colorbar()
+    plt.axis('off')
+    plt.show()
+
--- models.py
+++ models.py
@@ -3,6 +3,12 @@ import matplotlib.pyplot as plt
 #from tools.utils import logger
 
 
+
+#---------------------------------
+# ---| Network implementation |---
+#---------------------------------
+
+
 class Network:
 
     def __init__(self, N: int=1, Nj: int=1, **kwargs):
@@ -37,10 +43,12 @@ class Network:
         # parameters
         self.N = N
         self.Nj = Nj
-        self.tau = kwargs.get('tau', 10)
+        self.tau = kwargs.get('tau_u', 10)
         self.wr_const = kwargs.get('wr_const', 3)
         self.wff_const = kwargs.get('wff_const', 5)
         self.wff_max = kwargs.get('wff_max', 5)
+        self.wff_min = kwargs.get('wff_min', 0.05)
+        self.wff_tau = kwargs.get('wff_tau', 100)
 
         # state variables
         self.u = np.ones((N, 1)) * self._Erest
@@ -49,10 +57,21 @@ class Network:
         # connectivity
         self.Wff = np.ones((N, Nj)) / Nj * self.wff_const 
         self.Wr = (np.ones((N, N)) - np.eye(N)) * self.wr_const
-        self.clip_func = lambda x: (self.wff_max*np.exp(x) - self.wff_max*np.exp(-x)) / (np.exp(x) - np.exp(-x)).clip(min=0)
+
+        # input synapses
+        self._syn_ff = np.zeros((N, Nj))
+        self._syn_ff_tau = kwargs.get('syn_ff_tau', 10)
+        self._syn_ff_min = kwargs.get('syn_ff_min', 0.05)
+
+        # 
+        self._wff_beta = kwargs.get('wff_beta', 0.1)
+        self._wff_func = lambda x: np.exp(self._wff_beta * x) / np.exp(self._wff_beta * x).sum(axis=0)
 
         # rate function
         self.rate_func = lambda x: 1 / (1 + np.exp(-0.25 * (x + 58)))
+
+        # record
+        self.record = np.zeros((self.N, 3))
 
         print(self.__repr__())
 
@@ -71,20 +90,38 @@ class Network:
             Input from other neurons
         """
 
+        # weight decay
+        self.Wff += (self.wff_const / self.Nj - self.Wff) / self.wff_tau
+
         # Oja rule
         dWff = self.s * Sj.T - self.Wff @ (Sj * Sj) 
 
+        # FF synapses
+        self._syn_ff += (- self._syn_ff + dWff) / self._syn_ff_tau
+
         # update weights
-        self.Wff += self._lr * dWff
-
-        self.Wff = self.Wff.clip(min=0)
-
-        # clip weights
-        # self.Wff = self.clip_func(self.Wff)
+        self.Wff += self._lr * dWff * (np.abs(self._syn_ff) > self._syn_ff_min)
 
         # normalize weights
+        self._normalize()
+
+    def _normalize(self):
+
+        """
+        Normalize the weights 
+        """
+
+        # normalize weights across neurons
+        sofWff = self._wff_func(self.Wff)
+        self.Wff = sofWff / sofWff.max(axis=0) * self.Wff
+
+        # normalize weights within a node
         self.Wff = self.Wff / self.Wff.sum(axis=1, 
                                 keepdims=True) * self.wff_const
+
+        # clip weights
+        self.Wff = self.Wff.clip(min=self.wff_min,
+                                 max=self.wff_max)
 
     def step(self, Sj: np.ndarray):
 
@@ -97,8 +134,11 @@ class Network:
             Input from other neurons
         """
 
+        exc = self.Wff @ Sj
+        inh = self.Wr @ self.s
+
         # update state variables
-        self.u += (self._Erest - self.u) / self.tau + self.Wff @ Sj - self.Wr @ self.s
+        self.u += (self._Erest - self.u + exc - inh) / self.tau 
 
         # spike generation
         self.s = np.random.binomial(1, self.rate_func(self.u))
@@ -117,3 +157,82 @@ class Network:
         self.s = np.zeros((self.N, 1))
         self.Wff = np.ones((self.N, self.Nj)) / self.Nj * self.wff_const
 
+
+
+#--------------------------------
+# ---| Connectivity patterns |---
+#--------------------------------
+
+def mexican_hat_1D(N: int, A: int, B: int, sigma_exc: float, 
+                sigma_inh: float) -> np.ndarray:
+
+    """
+    Generate a Mexican hat connectivity pattern for N neurons.
+
+    Parameters
+    ----------
+    N : int
+        Number of neurons.
+    A : int
+        Amplitude of excitatory connections.
+    B : int
+        Amplitude of inhibitory connections.
+    sigma_exc : float
+        Standard deviation of excitatory connections.
+    sigma_inh : float
+        Standard deviation of inhibitory connections.
+
+    Returns
+    -------
+    W : np.ndarray
+        Connectivity matrix.
+    """
+
+    W = np.zeros((N, N))
+    for i in range(N):
+        for j in range(N):
+            d_ij = np.abs(i - j)
+            W[i, j] = A * np.exp(-d_ij**2 / (2 * sigma_exc**2)
+                                 ) - B * np.exp(
+                    -d_ij**2 / (2 * sigma_inh**2))
+
+    return W
+
+# Re-define the mexican hat weights function for 2D
+def mexican_hat_2D(dims: tuple, A: int, B: int, sigma_exc: int, sigma_inh: int) -> np.ndarray:
+
+    """
+    Generate a Mexican hat connectivity pattern for a 2D grid of neurons.
+
+    Parameters
+    ----------
+    dims : tuple
+        Dimensions of the grid (rows, columns).
+    A : int
+        Amplitude of excitatory connections.
+    B : int
+        Amplitude of inhibitory connections.
+    sigma_exc : int
+        Standard deviation of excitatory connections.
+    sigma_inh : int
+        Standard deviation of inhibitory connections.
+
+    Returns
+    -------
+    W : np.ndarray
+        Connectivity matrix.
+    """
+
+    rows, cols = dims
+    N = rows * cols
+    W = np.zeros((N, N))
+    for i in range(N):
+        for j in range(N):
+            # Convert index to 2D coordinates
+            x_i, y_i = i // cols, i % cols
+            x_j, y_j = j // cols, j % cols
+            # Calculate Euclidean distance
+            d_ij = np.sqrt((x_i - x_j)**2 + (y_i - y_j)**2)
+            W[i, j] = A * np.exp(-d_ij**2 / (2 * sigma_exc**2)) - B * np.exp(-d_ij**2 / (2 * sigma_inh**2))
+
+    return W
