83d0616ae16d3f854b44534aeb187963684bbae6
Krubeal <dkirubel@gmail.com>
2025-02-16 03:01:37 +0100

    fast update

--- src/evolution.py
+++ src/evolution.py
@@ -17,7 +17,7 @@ from game.constants import ROOMS, GAME_SCALE
 """ SETTINGS """
 logger = setup_logger(name="EVO", level=2, is_debugging=True, is_warning=True)
 
-NUM_SAMPLES = 10
+NUM_SAMPLES = 6
 ROOM_LIST = np.random.choice(ROOMS[1:], size=NUM_SAMPLES-2, replace=False).tolist() + \
     [ROOMS[0], ROOMS[0]]
 
