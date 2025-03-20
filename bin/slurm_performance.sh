4b291687ebac38096cc4307740d658d315622b62
Krubeal <dkirubel@gmail.com>
2025-03-20 17:41:30 +0100

    +performance ex3

--- bin/slurm_performance.sh
+++ bin/slurm_performance.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name="perfpc"
-#SBATCH -p roma16q #milanq #ipuq #milanq #armq #milanq #fpgaq #milanq # partition (queue)
+#SBATCH -p rome16q #milanq #ipuq #milanq #armq #milanq #fpgaq #milanq # partition (queue)
 #SBATCH -N 1 # number of nodes
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=64
+#SBATCH --cpus-per-task=32
 ##SBATCH --mem-per-cpu=1GB
 #SBATCH --time=0-23:00
 #SBATCH -o /home/daniekru/slurm.column.%j.%N.out # STDOUT
@@ -26,8 +26,8 @@ echo "[git 'main']"
 
 # --- RUN
 
-# srun python3 analysis/study_performance.py --reps 5 --cores 64 --save --room "Square.v0"
-srun python3 analysis/performance_comp.py --reps 12 --cores 64 --save --room "Flat.0010"
+# srun python3 analysis/study_performance.py --reps 5 --cores 32 --save --room "Square.v0"
+srun python3 analysis/performance_comp.py --reps 12 --cores 32 --save --room "Flat.0010" --load_idx 50
 
 echo "[finished]"
 
